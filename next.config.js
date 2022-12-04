8e63332c596b15bfca99623e102b1764aa7af507
ta93abe <ta93abe@ta93abe.com>
2022-12-04 16:34:10 +0900

    nextra setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+import Nextra from "nextra";
+
+const withNextra = Nextra({
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.tsx",
+	staticImage: true,
+	flexsearch: {
+		codeblocks: false,
+	},
+	defaultShowCopyCode: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	i18n: {
+		locales: ["en", "ja"],
+		defaultLocale: "ja",
+	},
 };
 
-export default nextConfig;
+export default withNextra(nextConfig);
---
+++ theme.config.tsx
@@ -0,0 +1,39 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs</span>,
+  project: {
+    link: "https://github.com/ta93abe/docs",
+  },
+  docsRepositoryBase: "https://github.com/ta93abe/docs/tree/main",
+  editLink: {
+    text: "",
+  },
+  feedback: {
+    content: () => <> Question? Give me feedback →</>,
+    labels: "feedback",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ta93abe'
+    }
+  },
+  primaryHue: 279,
+  i18n: [
+    { locale: "en", text: "English" },
+    { locale: "ja", text: "日本語" },
+  ],
+  footer: {
+    text: () => {
+      return (
+        <div>
+          <p className="mt-6 text-xs">
+            &copy; 2022 <a href="https://ta93a.be">ta93abe</a>
+          </p>
+        </div>
+      );
+    },
+  },
+};
+
+export default config;
